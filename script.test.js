15dff24606610762be6d033de461a0d8cc95f1eb
Kasper Mol <kaspermol1994@gmail.com>
2020-07-09 17:13:25 +0200

    Update to pull

--- script.test.js
+++ script.test.js
@@ -1,14 +1,13 @@
-const { wordpicker, wordList } = require("./script.js")
+const { wordpicker, wordList, letters } = require("./script.js")
 
 
 
 
 test("A word has been chosen", function () {
     expect(wordpicker(wordList)).not.toBeNull()
-}
-)
+})
 
 test("Does the word contain this letter", function () {
-
-
+    console.log("dit moet true zijn" + letters("toeter", ['e']))
+    expect(letters("toeter", [`e`])).toBeTruthy()
 })
